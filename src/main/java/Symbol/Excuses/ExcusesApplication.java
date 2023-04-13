26a0489ef7f1a1e6f3a1da836ddcb5d0417701de
Robin Hotton <Robin.hotton@hotmail.com>
2023-04-13 17:43:00 +0200

    build: initialisation de spring-boot

---
+++ src/main/java/Symbol/Excuses/ExcusesApplication.java
@@ -0,0 +1,13 @@
+package Symbol.Excuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcusesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Symbol/Excuses/ExcusesApplicationTests.java
@@ -0,0 +1,13 @@
+package Symbol.Excuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
