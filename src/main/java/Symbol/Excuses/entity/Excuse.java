1398e73c6820fa78664f57bbd8f1ed565db18295
Robin Hotton <Robin.hotton@hotmail.com>
2023-04-13 17:54:33 +0200

    feat: implementation de Excuse :sparkles:

--- src/main/java/Symbol/Excuses/entity/Excuse.java
+++ src/main/java/Symbol/Excuses/entity/Excuse.java
@@ -1,5 +1,61 @@
 package Symbol.Excuses.entity;
 
-public class Excuse {
+import jakarta.persistence.Entity;
 
+@Entity
+public class Excuse {
+    private int httpCode;
+    private String tag;
+    private String message;
+
+    public Excuse(int httpCode, String tag, String message) {
+        this.httpCode = httpCode;
+        this.tag = tag;
+        this.message = message;
+    }
+
+    public int getHttpCode() {
+        return httpCode;
+    }
+
+    public void setHttpCode(int httpCode) {
+        this.httpCode = httpCode;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Excuse)) return false;
+        if (!super.equals(object)) return false;
+        Excuse excuse = (Excuse) object;
+        return getHttpCode() == excuse.getHttpCode() && getTag().equals(excuse.getTag()) && getMessage().equals(excuse.getMessage());
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), getHttpCode(), getTag(), getMessage());
+    }
+
+    @Override
+    public String toString() {
+        return "Excuse{" +
+                "httpCode=" + httpCode +
+                ", tag='" + tag + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
