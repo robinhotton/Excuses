59f95fae96b4f906a1291ec7b78d353d0f68b700
Robin Hotton <Robin.hotton@hotmail.com>
2023-04-16 12:28:30 +0200

    feat: ajout d'un service et bientot supression hateoas :sparkles:

--- src/main/java/Symbol/Excuses/controller/ExcuseController.java
+++ src/main/java/Symbol/Excuses/controller/ExcuseController.java
@@ -1,12 +1,8 @@
 package Symbol.Excuses.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,73 +11,79 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-import Symbol.Excuses.ExcuseModelAssembler;
 import Symbol.Excuses.entity.Excuse;
-import Symbol.Excuses.repository.ExcuseRepository;
+import Symbol.Excuses.service.ExcuseService;
 
 @RestController
+@RequestMapping("/excuses")
 public class ExcuseController {
 
-	@Autowired
-	private ExcuseRepository repository;
-	private final ExcuseModelAssembler assembler;
+    @Autowired
+    private ExcuseService excuseService;
 
-	public ExcuseController(ExcuseRepository repository, ExcuseModelAssembler assembler) {
-		this.repository = repository;
-		this.assembler = assembler;
-	}
-
-	@GetMapping("/excuse")
-	public CollectionModel<EntityModel<Excuse>> getAllExcuses() {
-		List<EntityModel<Excuse>> excuse = repository.findAll().stream().map(assembler::toModel)
-				.collect(Collectors.toList());
-
-		return CollectionModel.of(excuse, linkTo(methodOn(ExcuseController.class).getAllExcuses()).withSelfRel());
-	}
-
-	@GetMapping("/excuse/{httpCode}")
-	public EntityModel<Excuse> getExcuseByHttpCode(@PathVariable int httpCode) {
-	    Excuse excuse = repository.findById(httpCode)
-	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Excuse not found"));
-
-	    return assembler.toModel(excuse);
-	}
-
-	@PostMapping("/excuse")
-	public ResponseEntity<?> ajoutExcuse(@RequestBody Excuse nouvelleExcuse) {
-		EntityModel<Excuse> entityModel = assembler.toModel(repository.save(nouvelleExcuse));
-
-		return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
-	}
-
-	@PutMapping("/excuse/{httpCode}")
-    public ResponseEntity<?> modifierExcuse(@RequestBody Excuse nouvelleExcuse, @PathVariable int httpCode) {
-    	Excuse updatedExcuse = repository.findById(httpCode)
-                .map(excuse -> {
-                	excuse.setMessage(nouvelleExcuse.getMessage());
-                	excuse.setTag(nouvelleExcuse.getTag());
-                    return repository.save(excuse);
-                })
-                .orElseGet(() -> {
-                	nouvelleExcuse.setHttpCode(httpCode);
-                    return repository.save(nouvelleExcuse);
-                });
-        EntityModel<Excuse> entityModel = assembler.toModel(updatedExcuse);
-
-        return ResponseEntity
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-                .body(entityModel);
+    // CREATE
+    @PostMapping
+    public ResponseEntity<Excuse> createExcuse(@RequestBody Excuse excuse) {
+        try {
+            Excuse createdExcuse = excuseService.createExcuse(excuse);
+            return new ResponseEntity<>(createdExcuse, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-	@DeleteMapping("/excuse/{httpCode}")
-	public ResponseEntity<?> SupprimerExcuse(@PathVariable int httpCode) {
-		repository.deleteById(httpCode);
-		return ResponseEntity.noContent().build();
-	}
+    // READ
+    @GetMapping("/{httpCode}")
+    public ResponseEntity<Excuse> getExcuseByHttpCode(@PathVariable("httpCode") int httpCode) {
+        try {
+            Excuse excuse = excuseService.getExcuseByHttpCode(httpCode);
+            if (excuse == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(excuse, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Excuse>> getAllExcuses() {
+        try {
+            List<Excuse> excuses = excuseService.getAllExcuses();
+            if (excuses.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(excuses, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // UPDATE
+    @PutMapping("/{httpCode}")
+    public ResponseEntity<Excuse> updateExcuse(@PathVariable("httpCode") int httpCode, @RequestBody Excuse excuse) {
+        try {
+            Excuse updatedExcuse = excuseService.updateExcuse(httpCode, excuse);
+            if (updatedExcuse == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(updatedExcuse, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // DELETE
+    @DeleteMapping("/{httpCode}")
+    public ResponseEntity<HttpStatus> deleteExcuse(@PathVariable("httpCode") int httpCode) {
+        try {
+            excuseService.deleteExcuseById(httpCode);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
---
+++ src/main/java/Symbol/Excuses/service/ExcuseService.java
@@ -0,0 +1,41 @@
+package Symbol.Excuses.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Symbol.Excuses.entity.Excuse;
+import Symbol.Excuses.repository.ExcuseRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ExcuseService {
+	
+	@Autowired
+    private ExcuseRepository excuseRepository;
+
+    public Excuse createExcuse(Excuse excuse) {
+        return excuseRepository.save(excuse);
+    }
+
+    public List<Excuse> getAllExcuses() {
+        return excuseRepository.findAll();
+    }
+
+    public Excuse getExcuseByHttpCode(int httpCode) {
+    	return excuseRepository.findById(httpCode).orElse(null);
+
+    }
+
+    public Excuse updateExcuse(int httpCode, Excuse newExcuse) {
+        Excuse existingExcuse = excuseRepository.findById(httpCode).orElseThrow(() -> new EntityNotFoundException("Excuse not found"));
+        existingExcuse.setTag(newExcuse.getTag());
+        existingExcuse.setMessage(newExcuse.getMessage());
+        return excuseRepository.save(existingExcuse);
+    }
+
+    public void deleteExcuseById(int httpCode) {
+        excuseRepository.deleteById(httpCode);
+    }
+}
